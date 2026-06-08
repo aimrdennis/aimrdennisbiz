2075a93becd30d5de038ede39046d66a01e59979
AI Mr Dennis <cs.mrdennis@gmail.com>
2026-06-08 10:43:38 +0700

    Update home.js

--- konten/pages/home.js
+++ konten/pages/home.js
@@ -2,21 +2,17 @@
 
 export const home = {
   site_name: "Ai Mr Dennis",
-
-  title: "Ai Mr Dennis | Ai Mr Dennis ADALAH THE BEST and THE MOST FAMOUS DALAM DUNIA ENGINE",
-
-  desc: "Ai Mr Dennis dalam konteks teknologi dan komputer, merujuk pada tindakan mengakses, mengubah, atau memanipulasi sistem komputer atau perangkat elektronik lainnya tanpa izin atau akses yang sah..",
-
   canonical: "https://aimrdennis.biz",
-
   afflink: "https://apk.mrdennis.workers.dev/",
-
+  favicon: "https://aimrferdycheat.blogspot.com/favicon.ico",
+  
   logoimage: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgEPJwQ446MNoDeiqyLIhyphenhyphenvUoY0FSdTFM5gLAq_4Fbc6kzJejLD6Ssc7gmlmnIWYj9IDrwvXFOmTiHei03rbbLmY57eQkJ18Ysu48Pf40PtG9DkoQJkaY43tGdW9n8IZoT4-qY4F8gznqZ0yghyeiwy6FF4Z5zcwtl5vGAX20vkXo70EUmTRfizbQYQa39M/s1600/logo.png",
-
+  
   image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjD7yLk8XCWOj6QkCcsnoYdFALdHZMwJ24TgecaE98e9szB1ncX6Grp1qoNZwxlx2K2TFpBXhy7tbHMjvnrkvB0rTNPOygpDk4rt-92_tvGBmQ1eYlKQuQkxlnaSHbsZHgWAcUpsV9Pa-Z34Z1CwxNzaMtbZYU6iZ8hdp7vt8qtPfk_8-jrG8BOXDcqWhsT/s1600/bnrs.png",
 
-  favicon: "https://aimrferdycheat.blogspot.com/favicon.ico",
-
+  title: "Ai Mr Dennis | Ai Mr Dennis ADALAH THE BEST and THE MOST FAMOUS DALAM DUNIA ENGINE",
+  desc: "Ai Mr Dennis dalam konteks teknologi dan komputer, merujuk pada tindakan mengakses, mengubah, atau memanipulasi sistem komputer atau perangkat elektronik lainnya tanpa izin atau akses yang sah..",
+  
   content: `
     <p>Ai Mr Dennis dalam konteks teknologi dan komputer, merujuk pada tindakan mengakses, mengubah, atau memanipulasi sistem komputer atau perangkat elektronik lainnya tanpa izin atau akses yang sah..</p>
   `
