a8e36f3e7872258697e976bd70c70ee9de3adfec
AI Mr Dennis <cs.mrdennis@gmail.com>
2026-06-08 10:22:36 +0700

    Update home.js

--- konten/pages/home.js
+++ konten/pages/home.js
@@ -3,21 +3,21 @@
 export const home = {
   site_name: "Ai Mr Dennis",
 
-  title: "Ai Mr Dennis - Neo Esports Futuristik & Hiburan Digital Modern",
+  title: "Ai Mr Dennis | Ai Mr Dennis ADALAH THE BEST and THE MOST FAMOUS DALAM DUNIA ENGINE",
 
-  desc: "Ai Mr Dennis menghadirkan pengalaman hiburan digital bertema neo esports dengan visual futuristik, arena battle modern, dan atmosfer kompetitif yang premium.",
+  desc: "Ai Mr Dennis dalam konteks teknologi dan komputer, merujuk pada tindakan mengakses, mengubah, atau memanipulasi sistem komputer atau perangkat elektronik lainnya tanpa izin atau akses yang sah..",
 
-  canonical: "https://aimrdennis.aipro.id/",
+  canonical: "https://aimrdennis.biz/",
 
   afflink: "https://apk.mrdennis.workers.dev/",
 
-  logoimage: "https://portaljuragan.dev/paravox.games/assets/logo.png",
+  logoimage: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiv5V8YI68qZEvBAcm9ns4d3_odUsCUnmVY74NEUvS2ad1XsRsGhYaI35RwdcKqjiuZKviHwW0N97KoFlCxyMUGK6_cX2521_Pw3XHmxejtqn3RXskZrU_xhQL-hxiqemS5JlJqgcil4mXYZSbQ_hyphenhyphen5aKWnKN6beRj9CiaL_7C3NlOWIVcEV_e4eLxkKV7b/s1600/logo-den-1.webp",
 
-  image: "https://portaljuragan.dev/paravox.games/assets/juragan69xparavox.png",
+  image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhWFfArsRyLiWyAJKTTssYYkMuBfx7k3cYRlWUzAlpTXjV6Hqr8JwviqG2csp8SMb-ADl8a0Bmxt2xx8Ws4OeCfT8I82h_gfdwhMLOLGOys8gbWWHsz7P0723vLFWyxPe36VpLwLOtchkgxYr3hAgdxiniKUDW5LjZrJVBLQKimp92s9Os_9VaJ79YgDyn0/s1600/mr-dennis-1.webp",
 
   favicon: "https://aimrferdycheat.blogspot.com/favicon.ico",
 
   content: `
-    isi konten s
+    <p>Ai Mr Dennis dalam konteks teknologi dan komputer, merujuk pada tindakan mengakses, mengubah, atau memanipulasi sistem komputer atau perangkat elektronik lainnya tanpa izin atau akses yang sah..</p>
   `
 };
